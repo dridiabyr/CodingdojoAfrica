a54eee0199946b790d1c79646a9c5de0f48b57f5
dridiabyr <dridiabir512@gmail.com>
2024-03-26 14:55:58 +0100

    prop-it-up

---
+++ MERN/React/Week2/Day2/prop-it-up/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import PersonCard from "./components/PersonCard";
+
+const people = [
+  { firstname: "Jane", lastname: "Doe", age: 30, hairColor: "Brown" },
+  { firstname: "john", lastname: "Smith", age: 25, hairColor: "Blonde" },
+  { firstname: "Fill", lastname: "millard", age: 40, hairColor: "Black" },
+  { firstname: "sarrah ", lastname: "Smith", age: 35, hairColor: "Red" },
+];
+
+function App() {
+  return (
+    <div className="App">
+      <h1> people</h1>
+      {people.map((person, index) => (
+        <PersonCard
+          key={index}
+          firstname={person.firstname}
+          lastname={person.lastname}
+          age={person.age}
+          hairColor={person.hairColor}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ MERN/React/Week2/Day2/prop-it-up/src/components/PersonCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PersonCard = ({ firstname, lastname, age, hairColor }) => {
+  return (
+    <div className="person">
+      <h2>{firstname}, {lastname} </h2>
+      <p> Age : {age} </p>
+      <p> Hair color : {hairColor} </p>
+    </div>
+  );
+};
+
+export default PersonCard;
