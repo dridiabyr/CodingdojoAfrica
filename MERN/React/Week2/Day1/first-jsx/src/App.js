8877b6735efc0ce85857af5e34a8e7c0dfe135f3
dridiabyr <dridiabir512@gmail.com>
2024-03-26 13:44:01 +0100

    first-jsx

---
+++ MERN/React/Week2/Day1/first-jsx/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello dojo </h1>
+      <h3> Things i need to do : </h3>
+     </div>
+  );
+}
+
+export default App;
