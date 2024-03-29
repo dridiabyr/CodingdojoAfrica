c85f403f66a409377675f876f51fc1f5cf622477
dridiabyr <dridiabir512@gmail.com>
2024-03-29 04:00:34 +0100

    box-generator

--- MERN/React/Week3/Day3/box-generator/src/Components/BoxeForm.jsx
+++ MERN/React/Week3/Day3/box-generator/src/Components/BoxeForm.jsx
@@ -2,37 +2,39 @@ import React, { useState } from "react";
 
 const BoxeForm = () => {
   const [color, setColor] = useState("");
+  const [dimensions, setDimensions] = useState({ width: "", height: "" });
   const [boxes, setBoxes] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (color !== '' ){
-        setBoxes([...boxes,color]);
-        setColor('')
-    }
 
+    const newBox = {
+      color: color,
+      width: dimensions.width,
+      height: dimensions.height,
     };
-
+    setBoxes([...boxes, color]);
+    setColor("");
+  };
 
   return (
-    <div className="bix-creator">
+    <div className="box-creator">
       <form onSubmit={handleSubmit}>
         <label> Color : </label>
-        <input
-          type="text"
-          value={color}
-          onChange={(e) => setColor(e.target.value)}
-        />
+        <input type="text" value={color} onChange={(e) => setColor(e.target.value)} />
 
+        <label>Width:</label>
+        <input type="number" value={dimensions.width} onChange={(e) => setDimensions({ ...dimensions, width: e.target.value })}/>
+        <label>heigth:</label>
+        <input type="number" value={dimensions.height} onChange={(e) => setDimensions({ ...dimensions, height: e.target.value }) }/>
         <button type="submit"> Add </button>
       </form>
+      
       <div className="box-container">
-        {boxes.map((boxColor, index) => (
-          <div
-            key={index}
-            className="box"
-            style={{ backgroundColor: boxColor }}
-          ></div>
+        {boxes.map((box, index) => (
+          <div key={index} className="box" style={{backgroundColor: box.color, width: `${box.width}px`,height: `${box.height}px`}}>
+
+          </div>
         ))}
       </div>
     </div>
